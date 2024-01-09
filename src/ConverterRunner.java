6095da23aa2f035f821a38c0628a72011ff4ec7b
DariuszSm <dariuszs2@nycstudents.net>
2024-01-09 14:01:39 -0500

    add extra digit values, progress on base conversions

--- src/ConverterRunner.java
+++ src/ConverterRunner.java
@@ -13,15 +13,15 @@ class ConverterRunner {
 
         System.out.print("Enter your number: ");
         String number = s.nextLine();
-        int n = Integer.parseInt(number);
 
         s.close();
 
-        NumberConverter nc = new NumberConverter(n, base);
+        NumberConverter nc = new NumberConverter(number, base);
         int[] digits = nc.getDigits();
         System.out.println("\n\nDigit array: " + Arrays.toString(digits));
         System.out.println("Number: " + nc.displayOriginalNumber());
-        System.out.println("Conv: " + Arrays.toString(nc.convertToDecimal()));
+        System.out.println("Conv: " + Arrays.toString(nc.convertToBase(10)));
+        System.out.println("Bina: " + Arrays.toString(nc.convertToBase(2)));
     }
 }
 
--- src/NumberConverter.java
+++ src/NumberConverter.java
@@ -2,12 +2,10 @@ public class NumberConverter {
     int[] digits;
     int base;
 
-    public NumberConverter(int number, int base) {
-        String numberAsString = Integer.toString(number);
-        digits = new int[numberAsString.length()];
-        for (int i = 0; i < numberAsString.length(); i++) {
-            String single = numberAsString.substring(i,i+1);
-            int d = Integer.parseInt(single);
+    public NumberConverter(String number, int base) {
+        digits = new int[number.length()];
+        for (int i = 0; i < number.length(); i++) {
+            int d = digitToValue(number.substring(i,i+1));
             digits[i] = d;
         }
         this.base = base;
@@ -26,20 +24,68 @@ public class NumberConverter {
         return digits;
     }
 
-    public int[] convertToDecimal() {
+    public int digitToValue(String digit) {
+        char digitChar = digit.charAt(0);
+        int digitCharInt = (int)digitChar;
+
+        if (digitCharInt >= 48 && digitCharInt <= 57) {
+            // if digit is a number value
+            return digitCharInt-48;
+        } else if (digitCharInt >= 65 && digitCharInt <= 90) {
+            // if digit is an uppercase letter
+            return digitCharInt-55;
+        } else if (digitCharInt >= 97 && digitCharInt <= 122) {
+            // if digit is an lowercase letter
+            return digitCharInt-61;
+        } else if (digitCharInt == 43) {
+            return 62;
+        } else if (digitCharInt == 47) {
+            return 63;
+        }
+
+        // invalid characters are 0
+        return 0;
+    }
+
+    public String valueToDigit(int value) {
+        if (value < 10) {
+            return String.valueOf(value);
+        } else if (value < 36) {
+            return String.valueOf((char)value+55);
+        } else if (value < 62) {
+            return String.valueOf((char)value+61);
+        } else if (value == 62) {
+            return "+";
+        } else if (value == 63) {
+            return "/";
+        }
+
+        // invalid characters are 0
+        return "0";
+    }
+
+    public int[] convertToBase(int base) {
         int decNum = 0;
         int[] decArray;
-        int numsUsed = 0;
 
+        // convert to base 10
         for (int i = 0; i < digits.length; i++) {
-            decNum += digits[digits.length - i - 1] * Math.pow(base, i);
+            decNum += digits[digits.length - i - 1] * Math.pow(this.base, i);
+            System.out.println(decNum);
+        }
+
+        int mutDecNum = decNum;
+        String mutDecStr = "";
+
+        for (int i = 0; i < String.valueOf(decNum).length(); i++) {
+            int quotient = mutDecNum/base;
+            mutDecStr += (mutDecNum % base) + "#";
         }
 
         String decStr = "";
 
         for (int i = 0; i < String.valueOf(decNum).length(); i++) {
             decStr += String.valueOf(decNum).substring(i, i+1) + "#";
-            numsUsed++;
         }
         System.out.println(decStr);
 
@@ -53,9 +99,21 @@ public class NumberConverter {
         return decArray;
     }
 
-    public int[] convertToBinary() {
-        int[] decArray = convertToDecimal();
-        String decString = "";
+    public int getInt() {
+        int[] decArray = convertToBase(10);
+        int decInt = 0;
+
+        for (int i = 0; i < decArray.length; i++) {
+            System.out.println(decArray[i] + ":" + (decArray.length-i));
+            decInt += decArray[i] * Math.pow(10, decArray.length-i-1);
+        }
+
+        return decInt;
+    }
+
+    public int[] convertToBase() {
+        int num = getInt();
+
         return null;
     }
 
